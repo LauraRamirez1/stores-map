f2c2503ae5e8ca08c6a2152eb592f930b9447c2b
LauraRamirez1 <laura.ramirez1@gmail.com>
2019-07-21 18:52:48 -0400

    Store register, modal and deploy

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import MexMap from './components/MexMap';
 function App() {
   return (
     <div>
-      <nav class="navbar navbar-light bg-light">
-        <span class="navbar-brand mb-0 h1">STORES-MAP</span>
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">STORES-MAP</span>
       </nav>
       <div className="row">
         <div className="col-8">
           <MexMap></MexMap>
         </div>
         <div className="col-4">
-          <h5>MIS TIENDAS FAVORITAS</h5>
+          <h5>FAVORITE STORES</h5>
         </div>
       </div>
     </div>
--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import {  Marker } from "react-google-maps";
 import stores from '../store_directory.json';
+import './Marker.css';
 
-class MarkerMap extends Component {     
+class MarkerMap extends Component {    
+    state = { upModal: false }
+
+    openModal = (name,address) => {
+      this.setState({ upModal: true, name, address});
+    }
+    
+    closeModal = () => {
+      this.setState({ upModal: false });
+      
+    } 
     render() {
       return (
         <div>
@@ -10,10 +21,32 @@ class MarkerMap extends Component {
             <Marker
               key={i}
               position={{ lat: element.Coordinates.lat, lng: element.Coordinates.lng }}
-              title={element.Name}
-            />))}
-        </div>
-      )
+              onClick={()=>this.openModal(element.Name,element.Address)}
+              />))}
+              <Modal 
+                upModal={this.state.upModal} 
+                handleClose={this.closeModal} >
+                <b id={this.props.key}>{this.state.name}</b>
+                <p>{this.state.address}</p>
+                <button type="button" className="btn btn-dark">Agregar a Favoritos </button>
+              </Modal>
+          </div>
+        )
+      }
     }
-  }
+    
+    const Modal = ({ handleClose, upModal, children }) => {
+      const stateModalClassName = upModal ? 'modal display-block' : 'modal display-none';
+      return (
+        <div className={stateModalClassName}>
+          <section className='containerModal'>
+            {children}
+            <button
+            type="button" className="btn btn-dark"
+              onClick={handleClose}>Cerrar
+            </button>
+          </section>
+        </div>
+      );
+    }
   export default MarkerMap;
